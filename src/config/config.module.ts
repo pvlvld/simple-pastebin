ce26de208bccfe27f1b2d05c4e5d12f11e3c3ab0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-17 21:11:25 +0300

    add database cfg fields to AppConfigModule

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -4,6 +4,11 @@ import Joi from 'joi';
 export type IAppConfig = Readonly<{
   NODE_ENV: 'development' | 'production';
   PORT: number;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USER: string;
+  DB_PASS: string;
+  DB_NAME: string;
 }>;
 
 export const AppConfigModule = ConfigModule.forRoot({
@@ -13,6 +18,11 @@ export const AppConfigModule = ConfigModule.forRoot({
       .valid('development', 'production')
       .default('development'),
     PORT: Joi.number().port().default(3000),
+    DB_HOST: Joi.string().required(),
+    DB_PORT: Joi.number().port().required(),
+    DB_USER: Joi.string().required(),
+    DB_PASS: Joi.string().required(),
+    DB_NAME: Joi.string().required(),
   }),
   validationOptions: {
     // allowUnknown: false,
