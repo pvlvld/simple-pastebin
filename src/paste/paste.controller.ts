acd120d4cfc5127917d527479a11ea7ae9561c00
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-18 18:19:40 +0300

    feat: implement paste post

--- src/paste/paste.controller.ts
+++ src/paste/paste.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { PasteService } from './paste.service';
 
 @Controller()
 export class PasteController {
+  constructor(private readonly pasteService: PasteService) {}
   @Get(':id')
   get(@Param('id') id: string): string {
     return `getting paste ${id}`;
   }
 
   @Post()
-  create(): string {
-    return 'creating paste';
+  async create(@Body() body: any): Promise<number> {
+    const paste = await this.pasteService.create(body);
+    return paste.id;
   }
 }
