a3aa70d05338fe95d145351f6271319fc2b35fe7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-18 18:10:25 +0300

    fix: dependency resolve, remove paste service/controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PasteController } from './paste/paste.controller';
-import { PasteService } from './paste/paste.service';
 import { PasteModule } from './paste/paste.module';
 import { AppConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PasteModule, AppConfigModule, DatabaseModule],
-  controllers: [AppController, PasteController],
+  imports: [AppConfigModule, DatabaseModule, PasteModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
