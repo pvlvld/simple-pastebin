a58ef0dc60dfae92c0f3517e17c8805e22bd5db9
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-19 19:43:49 +0300

    feat: integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  initializeSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
+
+function initializeSwagger(app: any) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Simple Pastebin API')
+    .setDescription('The Simple Pastebin API description')
+    .setVersion('1.0')
+    .addTag('paste')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+}
+
 bootstrap();
