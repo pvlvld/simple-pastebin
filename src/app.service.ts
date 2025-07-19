d9f9c8a7b930f4770712be03acd480c77441242d
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-07-19 21:22:52 +0300

    refactor: redirect to /api from /

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,4 @@ import type { IAppConfig } from './config/config.module';
 @Injectable()
 export class AppService {
   constructor(private configService: ConfigService<IAppConfig, true>) {}
-  getHello(): string {
-    const test = this.configService.get('NODE_ENV', { infer: true });
-    return `Hello World! Running in ${this.configService.get<{ NODE_ENV: string }>('NODE_ENV')} mode on port ${this.configService.get('PORT')}`;
-  }
 }
